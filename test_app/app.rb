704573fbc2630dd390a01b0220bc428f5a7a6c0d
Naoaki Yasuda <saisyohananabannaiann@yahoo.co.jp>
2017-09-09 09:51:36 +0000

    Add most

--- test_app/app.rb
+++ test_app/app.rb
@@ -5,7 +5,22 @@ get '/' do
   erb :index  
 end
 
-get '/count' do
-  @mojisuu = params[:num].length
+post '/count' do
+  if params[:most].nil? then
+    @return_value = params[:text].length
+  else
+    @input_value = params[:text]
+    @most_many_char_count = 0
+    @input_value.chars { |ch|
+      @input_value_char_count = @input_value.count(ch)
+      
+    if @most_many_char_count < @input_value_char_count then
+      @most_many_char_count = @input_value_char_count
+      @return_value = ch
+    end
+      
+    }
+  end
+
   erb :count
 end
