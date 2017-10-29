df640b9980ef578672bffad25c2b30a10e7bbe4f
Naoaki Yasuda <saisyohananabannaiann@yahoo.co.jp>
2017-10-29 02:39:32 +0000

    homework23'

--- test_app/app.rb
+++ test_app/app.rb
@@ -4,7 +4,6 @@ require 'pry'
 require 'sqlite3'
 
 
-
 def hash_sort_def(str)
   hash = {}
   str_ary = str.split(" ")
@@ -15,7 +14,7 @@ def hash_sort_def(str)
         hash[key] = 1
     end
   end
-  return hash_sort = hash.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
+  return hash.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
 end
 
 
@@ -26,27 +25,29 @@ end
 post '/add_to_database' do
 
   ua = request.env['HTTP_USER_AGENT']
-  if ua.include? "MSIE"
-    browser = "ie"
+  browser = if ua.include? "MSIE"
+    "ie"
   elsif ua.include? "Firefox"
-    browser = "firefox"
+    "firefox"
   elsif ua.include? "Chrome"
-    browser = "chrome"
+    "chrome"
   elsif ua.include? "Opera"
-    browser = "opera"
+    "opera"
   elsif ua.include? "safari"
-    browser = "safari"
+    "safari"
   else
-    browser = "others"
+    "others"
   end
+  
   db = SQLite3::Database.new('test.db')
   db.execute("insert into test (browser,text) values ('#{browser}','#{params[:foo]}')")
+  db.close
   
   return browser
 end
 
 post '/post' do
-  data = params[:foo].length.to_s
+  params[:foo].length.to_s
 end
 
 post '/most' do
@@ -67,6 +68,69 @@ input_value = params[:foo]
     }
     return @data
 end
+
+post '/browser_graph' do
+
+  wak = "<caption>単語数ランキング</caption><thead><tr><td></td>"
+
+  #hash = {"ie" => 0,"firefox" => 0,"chrome" => 0,"opera" => 0,"safari" => 0,others: 0 }
+  chrome_str_ary = []
+  others_str_ary = []
+  str_ary = []
+  
+  chrome_str_hash = {}
+  others_str_hash = {}
+  str_all = []
+  
+  db = SQLite3::Database.new('test.db')
+  db.results_as_hash = true
+  db.execute('select * from test') do |row|
+    str_ary = row["text"].split(" ")
+    str_ary.each do |key|
+      unless str_all.include?(key) then
+          str_all.push(key)
+        end
+      end
+  end
+  
+  db.execute('select * from test') do |row|
+    chrome_str_ary = row["text"].split(" ")
+    if row["browser"] == "chrome" then
+      str_all.each do |key|
+        if chrome_str_hash.has_key?(key) then
+          chrome_str_hash[key] += 1
+       else
+          chrome_str_hash[key] = 0
+        end
+      end
+    else
+     
+    end
+  end
+  db.close
+  
+  chrome_chars_sort = chrome_str_hash.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
+  #str_chars_sort = str_hash.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
+ 
+  def add_elements(wak, tag, close_tag, hash_sort, num)
+    hash_sort.each do |value|
+
+      wak += tag
+      wak += value[num].to_s
+      wak += close_tag
+    end
+    return wak
+  end
+
+  wak = add_elements(wak, "<th>", "</th>", chrome_chars_sort, 0)
+  wak += "</tr></thead><tbody><tr><th>単語数</th>\n"
+  wak = add_elements(wak, "<td>", "</td>", chrome_chars_sort, 1)
+  wak += "</tr></tbody>\n";
+  return wak
+  #trの中を繰り返す
+  
+end
+
 
 post '/bar_graph' do
 
