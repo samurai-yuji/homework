28a86ced4766e4e1010e1ee3ad562d3c837db4b9
Yuji Nishida <yuji@yuji.local>
2017-11-03 11:07:12 +0900

    Modified my code after self-review.

--- test_app/app.rb
+++ test_app/app.rb
@@ -70,84 +70,82 @@ input_value = params[:foo]
 end
 
 
-# Databaseとの接続インターフェース(ドライバ)を作成する
-def get_driver(file)
-  db = SQLite3::Database.new(file)
-  db.results_as_hash = true
-  return db
-end
+post '/browser_graph' do
 
-# ブラウザごとに単語数を示すハッシュを作成する
-# 単語数0の単語も表示するための全単語の配列も作成する
-def get_hash_array_for_browser(db)
-  hash = Hash.new { |h,k| h[k] = {} } # key:ブラウザ名, value:単語数を示すハッシュ
-  arr  = [] # 全単語の配列
+  # Databaseとの接続インターフェース(ドライバ)を作成する
+  def get_driver(file)
+    db = SQLite3::Database.new(file)
+    db.results_as_hash = true
+    return db
+  end
 
-  db.execute('select * from test') do |row|
-    word_cnt = hash[row["browser"]]
-    row["text"].split(" ").each do |str|
-      unless arr.include?(str) then
-        arr.push(str)
+  # ブラウザごとに単語数を示すハッシュを作成する
+  # 単語数0の単語も表示するための全単語の配列も作成する
+  def get_hash_array_for_browser(db)
+    hash = Hash.new { |h,k| h[k] = {} } # key:ブラウザ名, value:単語数を示すハッシュ
+    arr  = [] # 全単語の配列
+
+    db.execute('select * from test') do |row|
+      word_cnt = hash[row["browser"]]
+      row["text"].split(" ").each do |str|
+        unless arr.include?(str) then
+          arr.push(str)
+        end
+        if word_cnt.has_key?(str) then
+          word_cnt[str] += 1
+        else
+          word_cnt[str] = 1
+        end
       end
-      if word_cnt.has_key?(str) then
-        word_cnt[str] += 1
-      else
-        word_cnt[str] = 1
-      end 
+    end
+
+    return hash,arr
+  end
+
+  # 単語数0の単語を加える
+  def format_hash(hash,arr)
+    hash.each do |_,word_cnt| #各ブラウザで単語の順番を揃える
+      arr.each do |str|
+        unless word_cnt.has_key?(str) then
+          word_cnt[str] = 0
+        end
+      end
     end
   end
-
-  return hash,arr
-end
-
-# 単語数0の単語を加える
-def format_hash(hash,arr)
-  hash.each do |key,_| #各ブラウザで単語の順番を揃える
-    arr.each do |str|
-      unless hash[key].has_key?(str) then
-        hash[key][str] = 0
-      end  
-    end    
-  end
-end
-
-post '/browser_graph' do
 
   def add_elements(wak, tag, close_tag, word_cnt, flag)
     word_cnt.sort.each do |val|
       wak += tag
       case flag
-        when :word
-          wak += val[0].to_s
-        when :cnt
-          wak += val[1].to_s
-        else
+        when :word then wak += val[0].to_s
+        when :cnt  then wak += val[1].to_s
       end
       wak += close_tag
     end
     return wak
   end
 
+  def get_table_html(hash)
+    wak = "" # ?
+    hash.each_with_index do |(key,val),idx| #表の各行にブラウザ名と単語数を反映させる
+      if idx == 0
+        wak += "<caption>単語数ランキング</caption><thead><tr><td></td>"
+        wak = add_elements(wak, "<th>", "</th>", val, :word)
+      else
+        wak += "</tr></thead><tbody><tr><th>#{key}</th>\n"
+        wak = add_elements(wak, "<td>", "</td>", val, :cnt)
+      end
+    end
+    wak += "</tr></tbody>\n";
+	return wak
+  end
+
   db = get_driver('test.db')
   hash,arr = get_hash_array_for_browser(db)
   db.close
   format_hash(hash,arr)
-  
-  wak = "" # ?
-  hash.each_with_index do |(key,val),idx| #表の各行にブラウザ名と単語数を反映させる
-    if idx == 0
-      wak += "<caption>単語数ランキング</caption><thead><tr><td></td>"
-      wak = add_elements(wak, "<th>", "</th>", val, :word)
-    else
-      wak += "</tr></thead><tbody><tr><th>#{key}</th>\n"
-      wak = add_elements(wak, "<td>", "</td>", val, :cnt)
-    end
-  end
-  wak += "</tr></tbody>\n";
+  return get_table_html(hash)
 
-  return wak
-  #trの中を繰り返す (?)
-  
 end
 
 
